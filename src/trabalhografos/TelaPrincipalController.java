80bb38c025aa9957a0c6d8fb80596a9ce3dc8eeb
Aluno <Aluno@LF104m15-72129>
2019-08-09 11:52:12 -0300

    tela inicial(protótipo)

--- src/trabalhografos/TelaPrincipalController.java
+++ src/trabalhografos/TelaPrincipalController.java
@@ -5,12 +5,17 @@
  */
 package trabalhografos;
 
+import com.jfoenix.controls.JFXButton;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 
 /**
  *
@@ -18,10 +23,16 @@ import javafx.scene.control.Label;
  */
 public class TelaPrincipalController implements Initializable {
     
-    @FXML
     private Label label;
-    
     @FXML
+    private TableView<?> tbMA;
+    @FXML
+    private TableView<?> tbMI;
+    @FXML
+    private TableView<?> tbLista;
+    @FXML
+    private Pane painel;
+    
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
@@ -31,5 +42,14 @@ public class TelaPrincipalController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void evtClkMouse(MouseEvent event) 
+    {
+        JFXButton jb = new JFXButton("0");
+        jb.setLayoutX(event.getX());
+        jb.setLayoutY(event.getY());
+        painel.getChildren().add(jb);
+    }
     
 }
